f4d37806a78cec6be856ea61af5486bae49c2af7
Kenton Lam <kenton_lam@outlook.com>
2021-03-31 12:55:43 +1000

    School.

--- exercism/haskell/grade-school/src/School.hs
+++ exercism/haskell/grade-school/src/School.hs
@@ -1,15 +1,33 @@
 module School (School, add, empty, grade, sorted) where
 
-data School = Dummy
+import           Data.Foldable (toList)
+import           Data.List (sortOn, sort)
+import           Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as N
+
+data Student = Student { studentName :: String, studentGrade :: Int}
+  deriving (Eq, Show)
+
+newtype School = School { roster :: [Student] }
+  deriving (Eq, Show)
 
 add :: Int -> String -> School -> School
-add gradeNum student school = error "You need to implement this function."
+add g n s = School $ Student n g : roster s
 
 empty :: School
-empty = error "You need to implement this function."
+empty = School []
 
 grade :: Int -> School -> [String]
-grade gradeNum school = error "You need to implement this function."
+grade g = sort . fmap studentName . filter ((== g) . studentGrade) . roster
+
+flattenWithKey :: (a -> k) -> (a -> v) -> NonEmpty a -> (k, NonEmpty v)
+flattenWithKey fk fv as = (fk $ N.head as, fmap fv as)
+
+groupWithKey
+  :: Ord k => (a -> k) -> (a -> v) -> [a] -> [(k, NonEmpty v)]
+groupWithKey fk fv =
+  fmap (flattenWithKey fk fv) . N.groupBy (\a b -> fk a == fk b) . sortOn fk
 
 sorted :: School -> [(Int, [String])]
-sorted school = error "You need to implement this function."
+sorted s = fmap (sort . toList) <$> grouped
+  where grouped = groupWithKey studentGrade studentName (roster s)
