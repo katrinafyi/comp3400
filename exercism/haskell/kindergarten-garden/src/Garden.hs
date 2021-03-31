a81ee47b5b241d9f5b3fafd5d33191cbfc85ba47
Kenton Lam <kenton_lam@outlook.com>
2021-03-31 14:34:03 +1000

    Garden.

---
+++ exercism/haskell/kindergarten-garden/src/Garden.hs
@@ -0,0 +1,60 @@
+module Garden
+    ( Plant (..)
+    , garden
+    , lookupPlants
+    ) where
+
+import qualified Data.Map.Strict as M
+import Data.Maybe
+
+data Plant = Clover
+           | Grass
+           | Radishes
+           | Violets
+           deriving (Eq, Show)
+
+-- | A Garden is a Map of student names (as String) to a list of their Plants.
+newtype Garden = Garden (M.Map String [Plant]) deriving (Eq, Show)
+
+-- | Converts a character to the Plant if possible.
+toPlant :: Char -> Maybe Plant
+toPlant 'C' = Just Clover
+toPlant 'G' = Just Grass
+toPlant 'R' = Just Radishes
+toPlant 'V' = Just Violets
+toPlant _ = Nothing
+
+-- | Given a list, returns a list of list where each list has n elements.
+-- The final list in the returned list may have less than n if n does not
+-- evenly divide the input list's length.
+chunks :: Int -> [a] -> [[a]]
+chunks _ [] = []
+chunks n xs = take n xs : chunks n (drop n xs)
+
+-- | Applies a fold down the columns of a 2D list of lists.
+foldDown :: (a -> b -> b) -> b -> [[a]] -> [b]
+foldDown _ b [] = repeat b
+foldDown f b (a:as) = zipWith f a $ foldDown f b as
+
+-- | Transposes a 2D list (for fun).
+transpose :: [[a]] -> [[a]]
+transpose = foldDown (:) []
+
+-- | Given a list of students and a string representing plants, build the Garden.
+garden :: [String] -> String -> Garden
+garden s p = Garden (M.fromList pairs)
+  where
+    -- split plants into rows and convert to Plant.
+    rows = fmap toPlant <$> lines p
+    -- within each row, break into chunks of 2 plants each.
+    chunked = chunks 2 <$> rows
+    -- collect each student's plants down the colums by folding.
+    squares = foldDown (++) [] chunked
+    -- generate a list of (student, plants) tuples, then remove Nothing from
+    -- list of plants. we filter Nothing last so invalid plants early no do not
+    -- break the positioning of plants later in the same row.
+    pairs = fmap catMaybes <$> zip s squares
+
+-- | Returns the student's plants in the given garden.
+lookupPlants :: String -> Garden -> [Plant]
+lookupPlants s (Garden g) = M.findWithDefault [] s g
