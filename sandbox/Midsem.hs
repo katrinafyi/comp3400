69c96e01d9ce62cfa001b8ed31838c9154188f5d
Kenton Lam <kenton_lam@outlook.com>
2021-04-13 00:19:51 +1000

    (|||)

--- sandbox/Midsem.hs
+++ sandbox/Midsem.hs
@@ -3,6 +3,7 @@ module Midsem where
 import Data.Maybe ( isJust )
 import Data.Either (fromLeft)
 import Data.Ord
+import Control.Applicative
 
 -- q5
 
@@ -66,8 +67,58 @@ isJoker :: Card -> Bool
 isJoker Joker = True
 isJoker _ = False
 
-countAces :: [Card] -> Int
-countAces = length . filter ((||) <$> isAce <*> isJoker)
+-- we would like a function which returns true if a card is a joker or an ace
+f1, f2, f3, f4, f5, f6 :: Card -> Bool  -- (declaring functions in advance)
+f1 a = isJoker a || isAce a
+
+-- it's easy to spaceship this
+f2 = (||) <$> isJoker <*> isAce
+
+-- but this doesn't generalise well. ideally, we would like to have an or
+-- operator which works on functions, and arbitrarily many of them.
+or1, or2 :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+or1 f g a = f a || g a
+-- spaceships
+or2 f g = (||) <$> f <*> g
+
+-- but what we really want is to generalise a boolean operator like:
+-- (Bool -> Bool -> Bool) -> (a -> Bool) -> (a -> Bool) -> (a -> Bool)
+-- ... tada!
+(|||) :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+(|||) = liftA2 (||)
+infixr 2 |||
+
+f3 = isJoker ||| isAce
+f4 = isJoker ||| isAce ||| isJoker ||| isAce -- it generalises!
+
+-- ok cool, but what if we had a list of functions [a -> Bool]?
+-- well, looking at f4, it could become a list if we replace (|||) with (:).
+-- but that means we could just as easily replace (:) with (|||) with a fold.
+-- we also need a base which should be the identity for (|||), which is const True.
+f5 = foldr (|||) (const False) [isJoker, isAce]
+
+-- but there's a pattern here, all we've done is lift (||) and True into
+-- the applicative (->) a. in fact, you might remember:
+foldAp :: (Foldable f, Applicative g) => (a -> b -> b) -> b -> f (g a) -> g b
+foldAp f b = foldr (liftA2 f) (pure b)
+
+-- how good?
+f6 = foldAp (||) False [isJoker, isAce]
+
+-- this is pretty good but we've basically reimplemented "or" / "any".
+-- let's take a step back. we have a [a -> Bool] which is somewhat hard to work with.
+-- particularly, the (a ->) inside the list is inconvenient.
+-- what if we had a -> [Bool] instead? can we do that?
+
+-- of course we can. in fact, there is a very familiar prelude function to do it.
+g1 :: Card -> [Bool]
+g1 = sequence [isJoker, isAce]
+
+-- now, we have a list of Bool so we can just use or.
+f7 :: Card -> Bool
+f7 = or . sequence [isJoker, isAce]
+
+-- and we'll leave it at that.
 
 
 -- q10
