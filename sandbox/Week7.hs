dcf7f2e1b732da454308b8f1994f089a9013d3ef
Kenton Lam <kenton_lam@outlook.com>
2021-04-14 15:48:24 +1000

    |^|.

--- sandbox/Week7.hs
+++ sandbox/Week7.hs
@@ -28,16 +28,53 @@ f2 = contramap . contramap
 ex2'' :: (a -> b) -> ((a -> c) -> c) -> ((b -> c) -> c)
 ex2'' a b = getOp $ contramap Op $ f2 a (Op getOp C.. Op b)
 
-data a |*| b = Product a b deriving (Show)
+data a |*| b = Prod a b deriving (Show)
 data a |+| b = Sum1 a | Sum2 b deriving (Show)
 
-infix 9 |*|
-infix 8 |+|
+infixl 8 |*|
+infixl 7 |+|
 
 ex3 :: a |*| (b |+| c) -> (a |*| b) |+| (a |*| c)
-ex3 (Product a (Sum1 b)) = Sum1 (Product a b)
-ex3 (Product a (Sum2 c)) = Sum2 (Product a c)
+ex3 (Prod a (Sum1 b)) = Sum1 (Prod a b)
+ex3 (Prod a (Sum2 c)) = Sum2 (Prod a c)
 
 ex4 :: (a |*| b) |+| (a |*| c) -> a |*| (b |+| c)
-ex4 (Sum1 (Product a b)) = Product a (Sum1 b)
-ex4 (Sum2 (Product a c)) = Product a (Sum2 c)
+ex4 (Sum1 (Prod a b)) = Prod a (Sum1 b)
+ex4 (Sum2 (Prod a c)) = Prod a (Sum2 c)
+
+-- x ^ (a + b) = (x ^ a) ^ b
+
+{-
+What is a^b in types? With numbers, this is a * ... * a, b times.
+Consider:
+Ordering = LT | EQ | GT
+Bool = True | False
+
+How many functions are there Ordering -> Bool? For each of the 3 orderings,
+we have two possible values. Therefore, this is just 2 * 2 * 2 = 2^3.
+
+Observe that this is |Bool| ^ |Ordering|.
+
+arithmetic      types       logic
+---------------------------------
++               Either      ||
+*               (,)         &&
+^               (->)        ==>
+1               ()          True
+0               void        False (_|_)
+-}
+
+data a |^| b = Pow { getPow :: (b -> a) }
+
+ex5 :: c |^| (a |*| b) -> (c |^| a) |^| b
+ex5 (Pow f) = Pow (\b -> Pow (\a -> f (Prod a b)))
+
+ex5' :: ((a, b) -> c) -> (b -> a -> c)
+ex5' = flip . curry
+-- of course, we have an isomorphism from (a,b) to (b,a). this is swap.
+
+ex6 :: x |^| a |*| x |^| b -> x |^| (a |+| b)
+ex6 (Prod (Pow ax) (Pow bx)) = Pow $
+    \ab -> case ab of
+        Sum1 a -> ax a
+        Sum2 b -> bx b
