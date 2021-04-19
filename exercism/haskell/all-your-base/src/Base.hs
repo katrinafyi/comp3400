39e4923e277bb39c9ba237f6070c8804be21b7ed
Kenton Lam <kenton_lam@outlook.com>
2021-04-19 13:48:23 +1000

    Based.

--- exercism/haskell/all-your-base/src/Base.hs
+++ exercism/haskell/all-your-base/src/Base.hs
@@ -1,18 +1,22 @@
 module Base (Error(..), rebase) where
 
-import Data.Functor.Identity
-import Data.Foldable (Foldable(foldl'))
-import Data.Maybe (fromMaybe)
-import Data.List (unfoldr)
-import Data.Tuple (swap)
+import           Data.Foldable (Foldable(foldl'))
+import           Data.Maybe (fromMaybe)
+import           Data.List (unfoldr)
+import           Data.Tuple (swap)
 
-data Error a = InvalidInputBase | InvalidOutputBase | InvalidDigit a
-    deriving (Show, Eq)
+data Error a = InvalidInputBase
+             | InvalidOutputBase
+             | InvalidDigit a
+  deriving (Show, Eq)
 
-newtype Base a = Base a deriving (Show, Eq)
+newtype Base a = Base a
+  deriving (Show, Eq)
 
+-- | Based.
 -- coefficients in MOST significant first, e.g. 42 in base 10 is Based 10 [4,2].
-data Based a = Based { base :: Base a, coeffs :: [a] } deriving (Show, Eq)
+data Based a = Based { base :: Base a, coeffs :: [a] }
+  deriving (Show, Eq)
 
 liftBased :: ([a] -> [a]) -> Based a -> Based a
 liftBased f (Based b ns) = Based b (f ns)
@@ -21,31 +25,37 @@ type BasedEither a b = Either (Error a) b
 
 toBase :: Integral a => a -> Maybe (Base a)
 toBase b
-    | b > 1 = Just (Base b)
-    | otherwise = Nothing
+  | b > 1 = Just (Base b)
+  | otherwise = Nothing
 
 digitToBased :: Integral a => Base a -> a -> BasedEither a a
 digitToBased (Base b) n
-    | 0 <= n && n < b = Right n
-    | otherwise = Left (InvalidDigit n)
+  | 0 <= n && n < b = Right n
+  | otherwise = Left (InvalidDigit n)
 
 listToBased :: Integral a => Base a -> [a] -> BasedEither a (Based a)
 listToBased b = fmap (Based b) . traverse (digitToBased b)
 
 basedToNum :: Integral a => Based a -> a
 basedToNum = foldl' <$> go . base <*> const 0 <*> coeffs
-    where go :: Integral a => Base a -> a -> a -> a
-          go (Base b) rest n = b * rest + n
+  where
+    go :: Integral a => Base a -> a -> a -> a
+    go (Base b) rest n = b * rest + n
 
 numToBased :: Integral a => Base a -> a -> Based a
 numToBased b = liftBased reverse . Based b . unfoldr (go b)
-    where go :: Integral a => Base a -> a -> Maybe (a, a)
-          go _ 0 = Nothing
-          go (Base b) n = Just $ swap (n `quotRem` b)
+  where
+    go :: Integral a => Base a -> a -> Maybe (a, a)
+    go _ 0 = Nothing
+    go (Base b) n = Just $ swap (n `quotRem` b)
+
+maybeToEither :: a -> Maybe b -> Either a b
+maybeToEither a (Just b) = Right b
+maybeToEither a Nothing = Left a
 
 rebase :: Integral a => a -> a -> [a] -> BasedEither a [a]
 rebase inputBase outputBase inputDigits = do
-    i <- maybe (Left InvalidInputBase) Right $ toBase inputBase
-    o <- maybe (Left InvalidOutputBase) Right $ toBase outputBase
-    based <- listToBased i inputDigits
-    pure $ coeffs . numToBased o . basedToNum $ based
+  i <- maybeToEither InvalidInputBase $ toBase inputBase
+  o <- maybeToEither InvalidOutputBase $ toBase outputBase
+  based <- listToBased i inputDigits
+  pure $ coeffs . numToBased o . basedToNum $ based
