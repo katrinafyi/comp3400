026953d33b0a0305ce8d909e7431c5651e4a56ea
Kenton Lam <kenton_lam@outlook.com>
2021-04-21 14:56:45 +1000

    Series.

--- exercism/haskell/series/src/Series.hs
+++ exercism/haskell/series/src/Series.hs
@@ -1,10 +1,24 @@
 module Series (slices) where
 
+import Data.Char (digitToInt)
+
+takeExact :: Int -> [a] -> Maybe [a]
+takeExact 0 _ = Just []
+takeExact _ [] = Nothing
+takeExact n (x:xs)
+  | n < 0 = Nothing
+  | otherwise = (x:) <$> takeExact (n - 1) xs
+
+-- | Returns a sliding window of size n across the list.
+-- No sublists of length != n are returned; if the list contains less than n
+-- elements, the returned list will be empty.
+-- If n is negative, an empty list is returned.
+window :: Int -> [a] -> [[a]]
+window 0 [] = [[]]
+window _ [] = []
+window n (x:xs) = case takeExact n (x:xs) of
+  Just chunk -> chunk : window n xs
+  Nothing    -> []
+
 slices :: Int -> String -> [[Int]]
-slices n xs =
-  let
-    len = length xs
-    ints :: [Int]
-    ints = read . (:[]) <$> xs
-  in
-   (\i -> take n $ drop i ints) <$> [0..len - n]
+slices n = window n . fmap digitToInt
