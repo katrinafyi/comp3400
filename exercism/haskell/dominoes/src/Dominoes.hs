aef2723f854af54ee611fb98f6d263ed05b011a8
Kenton Lam <kenton_lam@outlook.com>
2021-05-03 21:36:30 +1000

    Formatting.

--- exercism/haskell/dominoes/src/Dominoes.hs
+++ exercism/haskell/dominoes/src/Dominoes.hs
@@ -1,9 +1,10 @@
 module Dominoes (chain) where
 
-import Data.List (find, delete)
-import Data.Tuple (swap)
+import           Data.List (find, delete)
+import           Data.Tuple (swap)
 
-newtype Domino = Domino { getDomino :: (Int, Int) } deriving Show
+newtype Domino = Domino { getDomino :: (Int, Int) }
+  deriving Show
 
 instance Eq Domino where
   Domino d1 == Domino d2 = d1 == d2 || d1 == swap d2
@@ -12,10 +13,11 @@ instance Eq Domino where
 isPair :: Domino -> Domino -> Bool
 isPair (Domino d1) (Domino d2) = snd d1 == fst d2
 
--- | Returns true if the given list of dominos forms a circular chain.
+-- | Returns true if the given list of dominos forms a full circular chain.
 isChain :: [Domino] -> Bool
 isChain ds = all (uncurry isPair) $ zip ds ds'
-    where ds' = drop 1 $ cycle ds
+  where
+    ds' = drop 1 $ cycle ds
 
 -- | Returns true if the number appears on the given domino.
 hasNumber :: Int -> Domino -> Bool
@@ -24,8 +26,8 @@ hasNumber n (Domino (x, y)) = n == x || n == y
 -- | If necessary, flips the domino so n appears in the left position.
 flipDomino :: Int -> Domino -> Domino
 flipDomino n (Domino d)
-    | fst d == n = Domino d
-    | otherwise = Domino (swap d)
+  | fst d == n = Domino d
+  | otherwise = Domino (swap d)
 
 -- | Returns all partial chains which could appear following the given first domino.
 -- The first domino is not contained in any of the returned chains.
@@ -33,10 +35,10 @@ flipDomino n (Domino d)
 -- "wraps around" to the first.
 chainFrom :: Domino -> [Domino] -> [[Domino]]
 chainFrom (Domino (_, n)) ds = do
-    first <- filter (hasNumber n) ds
-    let first' = flipDomino n first
-    let ds' = delete first ds  -- deletes using Domino's Eq isntance.
-    chainFromFirst (first':ds')
+  first <- filter (hasNumber n) ds
+  let first' = flipDomino n first
+  let ds' = delete first ds  -- deletes using Domino's Eq isntance.
+  chainFromFirst (first':ds')
 
 -- | Returns all partial chains which could be constructed from the given dominos.
 -- The first domino in the list is taken as the first domino of the chain (arbitrarily).
